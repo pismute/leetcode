36cb112f2f21ed33eec8bd4ece3298b0c86b3de2
Changwoo Park <pismute@gmail.com>
2023-06-12 18:43:57 +0200

    set up python env

---
+++ python/leetcode/0070_climbing_stairs.py
@@ -0,0 +1,39 @@
+class Solution:
+    """
+    n = 4
+    [1,2,3,4]
+                    1                       2
+             2            3           3          4
+         3      4                 4
+      4
+
+    brute force
+    O(2^n), O(n)
+
+    memoization
+    O(2n), O(n + n)
+
+    dp: bottom up
+    [1,2,3,4]
+     0,1,2,3 - index
+    [3 2 1 1] - answer 3
+    
+    O(n), O(2)
+
+    dp: top down
+     0 1 2 3
+    [1,1,2,3] - anwser 3
+    O(n), O(2)
+    
+    """
+    def climbStairs(self, n: int) -> int:
+        dp = [1, 1]
+
+        for i in range(2, n+1):
+            dp[i % 2] = dp[0] + dp[1]
+
+        return dp[n % 2]
+    
+if __name__ == '__main__':
+    assert Solution().climbStairs(2) == 2
+    assert Solution().climbStairs(3) == 3
