8b663e520dd610d924f94dcba3ac2e35533ed03f
Changwoo Park <pismute@gmail.com>
2023-06-13 06:37:24 +0200

    python: 509. Fibonacci Number

---
+++ python/leetcode/0509_fibonacci_number.py
@@ -0,0 +1,20 @@
+class Solution:
+    """
+    F(0) = 0, F(1) = 1
+    F(n) = F(n - 1) + F(n - 2), for n > 1.
+
+    O(n), O(2)
+    """
+    def fib(self, n: int) -> int:
+        dp = [0, 1]
+
+        for i in range(2, n+1):
+            dp[i % 2] = dp[0] + dp[1]
+
+        return dp[n % 2]
+    
+if __name__ == '__main__':
+    assert Solution().fib(2) == 1
+    assert Solution().fib(3) == 2
+    assert Solution().fib(4) == 3
+    
