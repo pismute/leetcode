1ae090aef4511c0603771f01d18a8767c8ae72dc
Changwoo Park <pismute@gmail.com>
2023-06-14 20:07:31 +0200

    python: 746. Min Cost Climbing Stairs

---
+++ python/leetcode/0746_min_cost_climbing_stairs.py
@@ -0,0 +1,44 @@
+from typing import List
+from array import array
+
+class Solution:
+    """
+    [1,100,1,1,1]
+                             i=0               i=1
+                      1            2       2          3
+                 2       3      3    4
+                    ...........................
+
+    brute force
+    O(2^n), O(n)
+
+    dp: bottom up
+
+    [1,100,1,1,1]
+     3 101 2 1 1
+
+    dp[0] = 3
+
+    O(n), O(2)
+
+    dp: top down
+
+    [1,100,1,1,1]
+     1,100,2,3,3
+
+    dp[n-1] = 3
+
+    O(n), O(2)
+    """
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        dp = array('i', [0, 0])
+
+        for i in range(len(cost)):
+            dp[i % 2] = min(dp) + cost[i]
+
+        return min(dp)
+    
+if __name__ == '__main__':
+    assert Solution().minCostClimbingStairs([1,100,1,1,1]) == 3
+    assert Solution().minCostClimbingStairs([10,15,20]) == 15
+    assert Solution().minCostClimbingStairs([1,100,1,1,1,100,1,1,100,1]) == 6
