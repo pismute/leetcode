0f4115efb35fdef219b0767c3233aee4b22c5d6e
Changwoo Park <pismute@gmail.com>
2023-06-22 12:03:25 +0200

    update rust: 78. Subsets

--- rust/src/bin/0078_subsets.rs
+++ rust/src/bin/0078_subsets.rs
@@ -23,35 +23,25 @@ fn main() {
 /*
  * [1,2,3]
  *
- *                           []
- *               []                              3,[3]
- *         []              2           [];p[3]                2;[3,2]
- *     []     1;[1] [];[2]   1;[2,1]           [];[3,2]             ;[3,2,1]
+ *                    1                   2                     3
+ *               2           3                 3
+ *               3
  *
  * O(2^n), O(n)
  */
 pub fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
-    let len = nums.len() * (nums.len() - 1) / 2;
+    fn go(i: usize, ss: &mut Vec<i32>, ns: &[i32], acc: &mut Vec<Vec<i32>>) {
+        acc.push(ss.clone());
 
-    fn go(i: usize, ss: &mut Vec<i32>, ns: &[i32], mut acc: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-        if i >= ns.len() {
-            acc.push(ss.clone());
-            acc
-        } else {
-            // include
-            ss.push(ns[i]);
-            let acc2 = go(i + 1, ss, ns, acc);
-
-            // not include
+        for j in i..ns.len() {
+            ss.push(ns[j]);
+            go(j + 1, ss, ns, acc);
             ss.pop();
-            go(i + 1, ss, ns, acc2)
         }
     }
 
-    go(
-        0,
-        &mut Vec::with_capacity(nums.len()),
-        &nums,
-        Vec::with_capacity(len),
-    )
+    let len = nums.len() * (nums.len() - 1) / 2;
+    let mut res = Vec::with_capacity(len);
+    go(0, &mut Vec::with_capacity(nums.len()), &nums, &mut res);
+    res
 }
