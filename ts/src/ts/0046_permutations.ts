12b5f2675714511b8fba9ac7ee75f34d992d5ec4
Changwoo Park <pismute@gmail.com>
2023-06-25 20:14:16 +0200

    ts: 46. Permutations

--- ts/src/ts/0046_permutations.ts
+++ ts/src/ts/0046_permutations.ts
@@ -9,7 +9,25 @@ import assertEqual from './assert';
  *
  */
 function permute(nums: number[]): number[][] {
-  return [[1]];
+  const res: number[][] = [];
+
+  function go(mask: number, cur: number[]) {
+    if (mask === ((1 << nums.length) - 1)) {
+      res.push(cur.slice());
+    } else {
+      for (let i = 0; i < nums.length; i++) {
+        const pos: number = 1 << i;
+        if ((mask & pos) === 0) {
+          cur.push(nums[i]);
+          go(mask | 1 << i, cur);
+          cur.pop();
+        }
+      }
+    }
+  }
+
+  go(0, []);
+  return res;
 };
 
 assertEqual(permute([1, 2, 3]), [[1, 2, 3], [1, 3, 2], [2, 1, 3], [2, 3, 1], [3, 1, 2], [3, 2, 1]]);
