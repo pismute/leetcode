6f6c7dbf88c5b3b128112a95802b9a4b32e6e1e2
Changwoo Park <pismute@gmail.com>
2023-06-25 12:06:19 +0200

    update rust: 47. Permutations II

--- rust/src/bin/0047_permutations2.rs
+++ rust/src/bin/0047_permutations2.rs
@@ -5,6 +5,17 @@ fn sort(mut vv: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
 
 fn main() {
     assert_eq!(
+        sort(permute_unique(vec![2, 2, 1, 1])),
+        sort(vec![
+            vec![1, 1, 2, 2],
+            vec![1, 2, 1, 2],
+            vec![1, 2, 2, 1],
+            vec![2, 1, 1, 2],
+            vec![2, 1, 2, 1],
+            vec![2, 2, 1, 1]
+        ])
+    );
+    assert_eq!(
         sort(permute_unique(vec![1, 1, 2])),
         sort(vec![vec![1, 1, 2], vec![1, 2, 1], vec![2, 1, 1]])
     );
@@ -27,50 +38,35 @@ fn main() {
  * brute force:
  * O(n! or n*2^n), O(n)
  *
- * immutable approach with HashMap
- *
  * 0  : ([], {1,1,2})
  * 1st: ([1], {1, 2}),                ([2], {1, 1})
  * 2nd: ([1,1], {2}), ([1,2], {1}),   ([2, 1], {1})
  * 3nd: ([1,1,2], {}), ([1,2,1], {}), ([2,1,1], {})
  *
- * O(n*2^n), O(n*n)
+ * O(n*2^n), O(n)
  */
-pub fn permute_unique(nums: Vec<i32>) -> Vec<Vec<i32>> {
-    type Map = std::collections::HashMap<i32, i32>;
-
-    fn remove(x: i32, mut m: Map) -> Map {
-        if let Some(v) = m.get_mut(&x) {
-            if *v > 1 {
-                *v -= 1;
-            } else {
-                m.remove(&x);
-            }
+pub fn permute_unique(mut nums: Vec<i32>) -> Vec<Vec<i32>> {
+    nums.sort();
+    fn go(cur: &mut Vec<i32>, mask: usize, ns: &[i32], acc: &mut Vec<Vec<i32>>) {
+        if cur.len() == ns.len() {
+            acc.push(cur.clone());
         } else {
-            m.remove(&x);
-        }
-
-        m
-    }
-
-    let map = nums.iter().fold(Map::new(), |mut acc, x| {
-        acc.entry(*x).and_modify(|c| (*c) += 1).or_insert(1);
-        acc
-    });
-
-    fn go(cur: Vec<i32>, m: Map) -> Vec<Vec<i32>> {
-        if m.is_empty() {
-            vec![cur]
-        } else {
-            let mut res = vec![];
-            for (k, _) in &m {
-                let mut next = cur.clone();
-                next.push(*k);
-                res.extend(go(next, remove(*k, m.clone())));
+            for i in 0..ns.len() {
+                let pos = 1 << i;
+                let prev_pos = 1 << (i - 1);
+                if mask & pos == 0 {
+                    if i > 0 && ns[i] == ns[i - 1] && mask & prev_pos != 0 {
+                        continue;
+                    }
+                    cur.push(ns[i]);
+                    go(cur, mask | pos, ns, acc);
+                    cur.pop();
+                }
             }
-            res
         }
     }
 
-    go(vec![], map)
+    let mut res = vec![];
+    go(&mut vec![], 0, &nums, &mut res);
+    res
 }
