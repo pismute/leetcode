ca6732a5c2456aaebafb9813271adf8e8c85afd9
Changwoo Park <pismute@gmail.com>
2023-06-26 12:29:43 +0200

    ts: 509. Fibonacci Number

---
+++ ts/src/ts/0509_fibonacci_number.ts
@@ -0,0 +1,20 @@
+import assertEqual from './assert';
+
+/*
+ */
+function fib(n: number): number {
+  const dp = [0, 1];
+
+  for (let i = 2; i <= n; i++) {
+    dp[i % 2] = dp.reduce((a, b) => a + b);
+  }
+
+  return dp[n % 2]
+};
+
+assertEqual(fib(2), 1);
+assertEqual(fib(3), 2);
+assertEqual(fib(4), 3);
+
+
+
